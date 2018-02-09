6e3345094fb953fe90cd02c0ed85b536042986a8
Mingjun <limmingjun@berkeley.edu>
2018-02-09 13:03:48 -0800

    first ruby hw!

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,9 @@
 class Foobar
 
   def self.baz(a)
-    # Class method, not an instance method
-    # Call with `Foobar.baz`
-    # Q4 CODE HERE
+    a.map! {|x| x.to_i}
+    a.map! {|x| x+2}
+    a.delete_if {|x| x%2 != 0 || x >= 10}
+    return a.uniq.inject(0, :+)
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,15 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  return (a+b) * (a+b)
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  return a.sort.map { |x| x+=1}
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return first_name + ' ' + last_name
 
 end
 
@@ -48,5 +48,7 @@ def scrabble(word)
     y: 4,
     z: 10,
   }
-  # Q5 CODE HERE
+  newarr = word.split('')
+  newarr.map! {|x| x.to_sym}
+  newarr.inject(0) {|result, x| result + values[x]}
 end
